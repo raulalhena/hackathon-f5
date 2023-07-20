8e744652a8f094efade8b24c8910fac1ee0f6f14
Ana <129515697+Alh-19@users.noreply.github.com>
2023-07-20 10:45:06 +0200

    Update DataContexProvider.js

--- frontend/src/Data/DataContexProvider.js
+++ frontend/src/Data/DataContexProvider.js
@@ -3,6 +3,24 @@ import React, { createContext } from 'react';
 export const DataContext = createContext();
 export function DataContextProvider({ children }) {
 
+    const getGlobalProducts = async () => {
+        const response = await fetch(`http://localhost:4000/products/search?filteredBy=&keyword=&sortedBy=createdAt`)
+        const result = await response.json()
+        console.log (result)
+
+        }; 
+    getGlobalProducts()
+
+    const getProductsByID = async (id) => {
+
+        const baseURL = "http://localhost:4000/products/"
+            const res = await fetch(`${baseURL}/${id}`)
+            const data = await res.json();
+            
+            return data
+    } 
+    getProductsByID()
+
 
     return (
         <DataContext.Provider>

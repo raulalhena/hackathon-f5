32e7855ca4aaa8bb770b8a5bd8ca1d4154849c39
Raul <raul.alhena@gmail.com>
2023-07-21 07:24:54 +0200

    checking before push

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import Product from './Product/Product';
-import Cards from './Cards/Cards';
 
 const SearchBar = () => {
   const [query, setQuery] = useState('');
